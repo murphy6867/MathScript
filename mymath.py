a1884f5cfdcbc4fde331b7398bc27aab00b8bbc0
Sonsawan Ngamsom <sonsawan.n1994@gmail.com>
2022-09-09 14:20:13 +0700

    Add come fucntion and fix

--- mymath.py
+++ mymath.py
@@ -1,9 +1,37 @@
+
+from math import factorial
+
+
 class mymath:
-    def __init__(self):
-        pass
-        
-    def proba2(num1, num2):
-        return (num1 * num2)
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        self.factorial = factorial
+    def add(self):
+        return (f"Answer {self.num1} + {self.num2} = {self.num1 + self.num2}")
     
-    def proba3(num1, num2, num3):
-        return (num1 * num2 * num3)
+    def minus(self):
+        return (f"Answer {self.num1} - {self.num2} = {self.num1 - self.num2}")
+    
+    def probability2(self):
+        return (f"Probability is : {self.num1 * self.num2}")
+    
+    def factorial(n):
+        if (n == 0):
+            return 0
+        elif (n == 1):
+            return 1
+        else:
+            return self.factorial(n - 1) + self.factorial(n - 2)
+        
+        
+def main():
+    x = float(input("Enter num 1 : "))
+    y = float(input("Enter num 2 : "))
+    
+    test = mymath(x, y)
+    
+    print(test.add())
+    
+if __name__ == "__main__":
+    main()
