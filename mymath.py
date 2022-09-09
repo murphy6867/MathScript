e3bf75d5969d6435f1539a8fb321dcea7b54c0e3
Sonsawan Ngamsom <sonsawan.n1994@gmail.com>
2022-09-09 09:59:52 +0700

    "Add 2 function Probability 2 and 3 variable"

--- mymath.py
+++ mymath.py
@@ -1,3 +1,11 @@
 class mymath:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, num1, num2, num3):
+        self.var1 = num1
+        self.var2 = num2
+        self.var3 = num3
+        
+    def proba2(num1, num2):
+        return (num1 * num2)
+    
+    def proba3(num1, num2, num3):
+        return (num1 * num2 * num3)
---
+++ test.py
@@ -0,0 +1,4 @@
+from mymath import mymath as mm
+
+
+print(mm.proba3(0.5, 0.5, 0.5))
