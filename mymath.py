1eac93728698a0375313324d5ecc1f9986167f28
Sonsawan Ngamsom <sonsawan.n1994@gmail.com>
2022-09-10 21:51:52 +0700

    "For fix fibo function"

--- mymath.py
+++ mymath.py
@@ -1,36 +1,38 @@
+from unittest import result
+
+
 class mymath:
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
-        
-    def add(self):
-        return (f"Answer {self.num1} + {self.num2} = {self.num1 + self.num2}")
+    def __init__(self):
+        pass
+    def add(num1, num2):
+        return (f"Answer {num1} + {num2} = {num1 + num2}")
     
-    def minus(self):
-        return (f"Answer {self.num1} - {self.num2} = {self.num1 - self.num2}")
+    def minus(num1, num2):
+        return (f"Answer {num1} - {num2} = {num1 - num2}")
     
-    def minus(self):
-        return (f"Answer {self.num1} - {self.num2} = {self.num1 - self.num2}")
+    def minus(num1, num2):
+        return (f"Answer {num1} - {num2} = {num1 - num2}")
     
-    def probability2(self):
-        return (f"Probability is : {self.num1 * self.num2}")
+    def probability2(num1, num2):
+        return (f"Probability is : {num1 * num2}")
     
-    def factorials(self):
-        if (self.num1 == 0):
+    def fibo(self, num1):
+        if (num1 < 0):
+            print("Input not corlected")
+        elif (num1 == 0):
             return 0
-        elif (self.num1 == 1):
+        elif (num1 == 1):
             return 1
         else:
-            return self.factorials(self.num1 - 1) * self.factorials(self.num1 - 2)
+            return self.fibo(num1 - 1) + self.fibo(num1 - 2)
         
         
 def main():
-    x = float(input("Enter num 1 : "))
-    y = float(input("Enter num 2 : "))
+    #x = float(input("Enter num 1 : "))
+    #y = float(input("Enter num 2 : "))
     
-    test = mymath(x, y)
-    
-    print(test.add())
+    #a = mymath()
+    print(mymath.add(7, 7))
     
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-from math import factorial
 from mymath import mymath as mm
 
 def main():
-    
-   #fac = mm.proba3(0.5, 0.5, 0.5)
-   #fac = mm.factorial(5)
-   print(factorial(5))
-   
-if __name__ == "__main__":
-    main()
+    a = mm()
+    print(a.fibo(6))
+
+main()
